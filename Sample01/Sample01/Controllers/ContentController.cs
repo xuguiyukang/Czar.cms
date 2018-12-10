59b5b3d1b5645dc036ca2ee969afc281a93221c6
xuguiyukang <yatb_kx@qq.com>
2018-12-10 18:18:34 +0800

    测试代码

---
+++ Sample01/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new ContentViewModel
+            {
+                Contents = new List<Content> { contents }
+            });
+
+            //var contents = new List<Content>();
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    contents.Add(new Content
+            //    {
+            //        id = i,
+            //        title = $"{i}的标题",
+            //        content = $"{i}的内容",
+            //        status = "1",
+            //        add_time = DateTime.Now.AddDays(-1)
+            //    });
+            //}
+
+            //return View(new ContentViewModel() { Contents = contents });
+        }
+    }
+}
---
+++ Sample01/Sample01/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Sample01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            IWebHostBuilder whb = CreateWebHostBuilder(args);
+            IWebHost wh = whb.Build();
+            wh.Run();
+
+            /*CreateWebHostBuilder(args).Build().Run();*/
+        }
+        /*
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+                    WebHost.CreateDefaultBuilder(args)
+                        .UseStartup<Startup>();
+                        */
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            IWebHostBuilder whb = WebHost.CreateDefaultBuilder(args);
+            whb.ConfigureAppConfiguration((a, b) =>
+            {
+                var env = a.HostingEnvironment;
+                b.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true).AddJsonFile("Content.json", optional: false, reloadOnChange: false).AddEnvironmentVariables();
+
+            });
+            return whb.UseStartup<Startup>();
+        }
+
+    }
+}
